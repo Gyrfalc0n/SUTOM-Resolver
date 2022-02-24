2920b0219c9093df316e96e36992d8a173e1303c
Gyrfalcon <46728024+Gyrfalc0n@users.noreply.github.com>
2022-02-24 16:45:40 +0100

    update phantomjs

--- resolver.py
+++ resolver.py
@@ -13,9 +13,11 @@ from selenium.webdriver.common.keys import Keys
 # -- VARIABLES --
 url = "https://sutom.nocle.fr/"
 chromedriver_path = 'C:\\Program Files\\chromedriver_win32\\chromedriver.exe'
+phantomjs_path = 'phantomjs-2.1.1/bin'
 
 # -- CORE --
-driver = webdriver.Chrome(executable_path=chromedriver_path)
+driver = webdriver.PhantomJS(executable_path=phantomjs_path)
+#driver = webdriver.Chrome(executable_path=chromedriver_path)
 action = ActionChains(driver)
 driver.get(url)
 #driver.maximize_window()
